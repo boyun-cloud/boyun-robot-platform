cdb489177c46f63eaeaeee241bfd14045dfd4b71
marklin <2273990296@qq.com>
2025-02-26 19:53:46 +0800

    commit to project

---
+++ boyun-robot-web/boyun-robot-openapi-web/src/main/java/com/boyun/cloud/openapi/api/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @className: com.boyun.cloud.openapi.api.package-info
+ * @projectName: 项目
+ * @module: 项目-package-info类，主要位于模块
+ * @content:
+ * @author: Powered by Marklin
+ * @datetime: 2025-02-26 19:53
+ * @version:
+ * @copyright: Copyright © 2018-2025  Systems Incorporated. All rights reserved.
+ */
+package com.boyun.cloud.openapi.api;
---
+++ boyun-robot-web/boyun-robot-openapi-web/src/main/java/com/boyun/cloud/openapi/web/infars/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @className: com.boyun.cloud.console.web.infars.package-info
+ * @projectName: 项目
+ * @module: 项目-package-info类，主要位于模块
+ * @content:
+ * @author: Powered by Marklin
+ * @datetime: 2025-02-26 19:45
+ * @version:
+ * @copyright: Copyright © 2018-2025  Systems Incorporated. All rights reserved.
+ */
+package com.boyun.cloud.openapi.web.infars;
