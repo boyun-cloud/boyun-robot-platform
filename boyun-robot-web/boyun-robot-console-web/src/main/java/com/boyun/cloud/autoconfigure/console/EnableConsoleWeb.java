9edab75fafd18f342b7f695aea49f02f73adf024
marklin <2273990296@qq.com>
2025-02-26 19:29:36 +0800

    commit to project

---
+++ boyun-robot-web/boyun-robot-console-web/src/main/java/com/boyun/cloud/autoconfigure/console/EnableConsoleWeb.java
@@ -0,0 +1,20 @@
+package com.boyun.cloud.autoconfigure.console;
+
+
+import java.lang.annotation.*;
+
+/**
+ * @className: com.boyun.cloud.autoconfigure.console.EnableConsoleWeb
+ * @projectName: 封装BoyunCloud项目-EnableAutoConsoleWeb类
+ * @module: BoyunCloud项目-EnableAutoConsoleWeb类，主要位于BoyunCloud项目Web模块Console工程模块的业务场景
+ * @content: EnableAutoConsoleWeb类，主要用于完成EnableConsoleWeb-后台管控平台的封装和定义.
+ * @author: Powered by Marklin
+ * @datetime: 2024-06-19 15:48
+ * @version: 1.0.0
+ * @copyright: Copyright © 2018-2024 BoyunCloud Systems Incorporated. All rights reserved.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface EnableConsoleWeb {
+}
---
+++ boyun-robot-web/boyun-robot-console-web/src/main/java/com/boyun/cloud/console/ConsoleWebApplication.java
@@ -0,0 +1,22 @@
+package com.boyun.cloud.console;
+
+import com.boyun.cloud.autoconfigure.console.EnableConsoleWeb;
+
+
+/**
+ * @className: com.boyun.cloud.console.ConsoleWebApplication
+ * @projectName: 封装BoyunCloud项目-ConsoleWebApplication类
+ * @module: BoyunCloud项目-ConsoleWebApplication类，主要位于BoyunCloud项目后台管控平台Web工程模块的业务场景
+ * @content: ConsoleWebApplication类，主要用于完成ConsoleWeb后台管控平台启动Application主类的封装和定义.
+ * @author: Powered by Marklin
+ * @datetime: 2024-06-18 12:12
+ * @version: 1.0.0
+ * @copyright: Copyright © 2018-2024 BoyunCloud Systems Incorporated. All rights reserved.
+ */
+@EnableConsoleWeb
+public class ConsoleWebApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
