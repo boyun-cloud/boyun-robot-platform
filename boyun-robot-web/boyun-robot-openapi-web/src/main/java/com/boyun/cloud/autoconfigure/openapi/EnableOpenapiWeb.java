e43015cf13abe4ce2e8b02586560262f0e6c5591
marklin <2273990296@qq.com>
2025-02-26 19:52:06 +0800

    commit to project

--- boyun-robot-web/boyun-robot-openapi-web/src/main/java/com/boyun/cloud/autoconfigure/openapi/EnableOpenapiWeb.java
+++ boyun-robot-web/boyun-robot-openapi-web/src/main/java/com/boyun/cloud/autoconfigure/openapi/EnableOpenapiWeb.java
@@ -3,7 +3,7 @@ package com.boyun.cloud.autoconfigure.openapi;
 import java.lang.annotation.*;
 
 /**
- * @className: com.boyun.cloud.autoconfigure.console.EnableOpenapiWeb
+ * @className: com.boyun.cloud.autoconfigure.openapi.EnableOpenapiWeb
  * @projectName: 封装BoyunCloud项目-EnableOpenapiWebb类
  * @module: BoyunCloud项目-EnableOpenapiWebb类，主要位于BoyunCloud项目Web模块Openapi工程模块的业务场景
  * @content: EnableOpenapiWebb类，主要用于完成EnableOpenapiWebb-开放平台的封装和定义.
