a0b843cbe60ca3f16209a49e80ee8e56e429930e
marklin <2273990296@qq.com>
2025-03-06 11:49:28 +0800

    commit to project

---
+++ boyun-robot-domain/boyun-robot-identity-domain/src/main/java/com/boyun/cloud/identity/domain/tenant/entity/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @className: com.boyun.cloud.identity.domain.account.entity.package-info
+ * @projectName: 项目
+ * @module: 项目-package-info类，主要位于模块
+ * @content:
+ * @author: Powered by Marklin
+ * @datetime: 2025-03-06 11:46
+ * @version:
+ * @copyright: Copyright © 2018-2025  Systems Incorporated. All rights reserved.
+ */
+package com.boyun.cloud.identity.domain.tenant.entity;
